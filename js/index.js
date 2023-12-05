a117a0ce5fcbd5502214a5842b156499c14574c1
Niko Cremer <n.ko@posteo.de>
2023-12-05 10:50:21 +0100

    set up

--- js/index.js
+++ js/index.js
@@ -41,6 +41,34 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.dev/api/people");
+  const data = await response.json();
+  console.log(data);
+  // ?
 }
+
+/*
+Have a look at the [`js/index.js`](./js/index.js) file: 
+There is an `EXAMPLE_DATA` object with information about
+ Luke Skywalker. We use this object to create a DOM element with the `Card()` function and append this element to the DOM with the help of `renderElement`.
+
+This approach is very static and not very handy. Lets get more data and create new cards from them dynamically!
+
+### Task 1: Fetch data from the Star Wars Api:
+
+Fetch data from the star wars api (https://swapi.dev/api/people).
+
+Use the `async` and `await` keywords to receive the response and request the data with the `.json() method`.
+Use a `console.log()` to look at the received data!
+
+### Task 2: Moooooooooore cards!
+
+Use an array method to create and render a card for each object in the fetched data array.
+
+The following hints may guide you:
+
+- What was the array method again to perform an action **for each** element?
+- You can use the functions `Card()` and `renderElement()` to create those cards, see the explanation above!
+
+Please switch to the [`js/index.js`](./js/index.js) file to start counting! */
